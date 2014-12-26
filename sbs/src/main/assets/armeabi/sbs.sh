0def47ffa1cb49603b7e20d5bfd7f582885b2b7a
Fredrik Markstrom <fredrik.markstrom@gmail.com>
2014-12-26 17:13:15 +0100

    Cleaned up script a bit

--- sbs/src/main/assets/armeabi/sbs.sh
+++ sbs/src/main/assets/armeabi/sbs.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-set -x
+#set -x
 set -e
 
 BINDIR=${0%sbs.sh}
@@ -7,7 +7,7 @@ LOG=$BINDIR/sbs-log.txt
 
 log () {
     /system/bin/log -t SBS $*
-    echo $*
+    echo $* >> $LOG
 }
 
 # Busybox comands
@@ -44,10 +44,10 @@ fi
 # Make sure the log is readable by the email client (and everyone else)
 test -e $LOG && bbchmod 0666 $LOG
 # Redirect all output to the log file
-exec 1>> $LOG 2>&1
+#exec 1>> $LOG 2>&1
 
 log "============="
-bbdate
+log "$((bbdate))"
 log "ARGS: $*"
 
 BB=$BINDIR/busybox
@@ -56,118 +56,175 @@ VER=$(getprop ro.build.version.release)
 CMVER=$(getprop ro.cm.version)
 
 log "VER:$VER CMVER:$CMVER"
-echo "$VER"   | grep "^4.4" && IS44=1 > /dev/null
-echo "$CMVER" | grep "^11" && ISCM11=1 > /dev/null
+echo "$VER"   | grep "^4.4" > /dev/null && IS44=1
+echo "$VER"   | grep "^5.0" > /dev/null && IS50=1
+echo "$CMVER" | grep "^11"  > /dev/null && ISCM11=1
 
-log "Got IS44=$IS44 ISCM11=$ISCM11"
-if [ -z "$LIBSF" ] ; then
+log "Got IS44=$IS44 IS50=$IS50 ISCM11=$ISCM11"
+if [ -z "$VARIANT" ] ; then
     if [ "$ISCM11" = "1" ] ; then
-	LIBSF=libsurfaceflinger-cm11.so
+	  VARIANT=cm11m9
     elif [ "$IS44" = "1" ] ; then
-	LIBSF=libsurfaceflinger-aosp-4.4.so
-else
-	log "No matching shared library" ; exit 108
+	  VARIANT=aosp444-102
+    elif [ "$IS50" = "1" ] ; then
+	  VARIANT=aosp500
+    else
+	    log "No matching shared library"
+	    exit 108
     fi
 fi
 
-log "Selected library: $LIBSF"
+log "Selected variant: $VARIANT"
 
 test -e $BB || ( log "Failed to copy busybox to $BB" 2>&1 ; exit 104 )
 
-case $1 in
-    install)
-        log "Installing"
-        bbmkdir /data/system/sbs
-        bbrm -f /data/system/sbs/libsurfaceflinger.so
-        bbln -s /data/data/com.frma.sbs/lib/$LIBSF /data/system/sbs/libsurfaceflinger.so
+function cmd_install () {
+    if [ -e /system/bin/surfaceflinger.real ] ; then
+        log "/system/bin/surfaceflinger.real already exists, please uninstall first"
+        exit 1
+    fi
+    bbmkdir /data/system/sbs
 
-        log "Modifying /system"
-        if [ -e /system/bin/surfaceflinger.real ] ; then
-            log "/system/bin/surfaceflinger.real already exists, please uninstall first"
-            exit 1
-        fi
-        bimount -o remount,rw /system
-        bbcp /system/bin/surfaceflinger /data/data/com.frma.sbs/files/surfaceflinger.org
-        bbmv /system/bin/surfaceflinger /system/bin/surfaceflinger.real
-        bbcp /data/data/com.frma.sbs/files/surfaceflinger /system/bin/surfaceflinger
-        bbchmod 755 /system/bin/surfaceflinger
-        bimount -o remount,ro /system
-        log "Done modifying system"
-         ;;
-    uninstall)
+    log "Modifying /system"
+    bimount -o remount,rw /system
+
+    log "Installing copying selected ($VARIANT) binaries into /system/lib/sbs"
+    bbmkdir /system/lib/sbs
+    bbcp /data/data/com.frma.sbs/files/libsurfaceflinger-$VARIANT.so /system/lib/sbs/libsurfaceflinger.so
+    bbcp /data/data/com.frma.sbs/files/surfaceflinger-$VARIANT       /system/lib/sbs/surfaceflinger
+    bbchmod 644 /system/lib/sbs/libsurfaceflinger.so
+    bbchmod 755 /system/lib/sbs/surfaceflinger
+
+    log "Replacing /system/bin/surfaceflinger with wrapper"
+    bbcp /system/bin/surfaceflinger /data/data/com.frma.sbs/files/surfaceflinger.org
+    bbmv /system/bin/surfaceflinger /system/bin/surfaceflinger.real
+    bbcp /data/data/com.frma.sbs/files/surfaceflinger /system/bin/surfaceflinger
+    bbchmod 755 /system/bin/surfaceflinger
+    bimount -o remount,ro /system
+    log "Done modifying system"
+}
+
+function cmd_uninstall () {
+    if [ ! -e /system/bin/surfaceflinger.real ] ; then
+        log "/system/bin/surfaceflinger.real not found, can't uninstall"
+    else
         rm -rf /data/system/sbs
-        if [ ! -e /system/bin/surfaceflinger.real ] ; then
-            log "/system/bin/surfaceflinger.real not found, can't uninstall"
-        else
-            bimount -o remount,rw /system
-            mv /system/bin/surfaceflinger.real /system/bin/surfaceflinger
-            bimount -o remount,ro /system
-        fi
-        ;;
-    isinstalled)
+        bimount -o remount,rw /system
+        mv /system/bin/surfaceflinger.real /system/bin/surfaceflinger
+        rm -rf /system/lib/sbs
+        bimount -o remount,ro /system
+    fi
+}
+
+function cmd_isinstalled () {
         if [ -e /system/bin/surfaceflinger.real ] ; then
             log "Result: Yes"
-            exit 0
+            return 0
         else
             log "Result: No"
-            exit 1
+            return 1
         fi
-	    ;;
-    enable)
-        bbmkdir /data/system/sbs
-        bbtouch /data/system/sbs/enabled
-        ;;
-    enablepermanent)
-        bbmkdir /data/system/sbs
-        bbtouch /data/system/sbs/permanent
-        ;;
-    disable)
-        bbrm -f /data/system/sbs/enabled
-        bbrm -f /data/system/sbs/permanent
-        ;;
-    ispermanent)
-        if [ -e /data/system/sbs/permanent ] ; then
-            log "Result: Yes"
-            exit 0
-        else
-            log "Result: No"
-            exit 1
-        fi
-        ;;
-    isenabled)
-        if [ -e /data/system/sbs/enabled ] ; then
-            log "Result: Yes"
-            exit 0
-        else
-            log "Result: No"
-            exit 1
-        fi
-        ;;
-    isloaded)
-        if grep com.frma.sbs /proc/$(bbpidof surfaceflinger.real)/maps > /dev/null 2>&1 ; then
-            log "Result: Yes"
-            exit 0
-        else
-            log "Result: No"
-            exit 1
-        fi
-	    ;;
-	reboot)
-	    sync
-	    ( stop ; reboot ) &
-	    ;;
-    set)
-        FLAGS=$2
-        ZOOM=$3
-        IMGDIST=$4
+}
+
+function cmd_enable () {
+    bbmkdir /data/system/sbs
+    bbtouch /data/system/sbs/enabled
+}
+
+function cmd_enable_permanent () {
+    bbmkdir /data/system/sbs
+    bbtouch /data/system/sbs/permanent
+}
+
+function cmd_disable () {
+    bbrm -f /data/system/sbs/enabled
+    bbrm -f /data/system/sbs/permanent
+}
+
+function cmd_ispermanent () {
+    if [ -e /data/system/sbs/permanent ] ; then
+        log "Result: Yes"
+        return 0
+    else
+        log "Result: No"
+        return 1
+    fi
+}
+
+function cmd_isenabled () {
+    if [ -e /data/system/sbs/enabled ] ; then
+        log "Result: Yes"
+        return 0
+    else
+        log "Result: No"
+        return 1
+    fi
+}
+
+function cmd_isloaded () {
+    if grep /system/lib/sbs /proc/$(bbpidof surfaceflinger.real)/maps > /dev/null 2>&1 ; then
+        log "Result: Yes"
+        return 0
+    else
+        log "Result: No"
+        return 1
+    fi
+}
+
+function cmd_reboot () {
+    sync
+    ( stop ; reboot ) &
+}
+
+function cmd_set () {
+        FLAGS=$1
+        ZOOM=$2
+        IMGDIST=$3
         R=$(service call SurfaceFlinger 4711 i32 $FLAGS i32 $ZOOM i32 $IMGDIST)
 	    log "Result: $R"
 	    echo "$R" | grep "^Result: Parcel(NULL)" && ( echo "Ok" ; exit 0 ) || ( log "Not installed" ; exit 106)
+}
+case $1 in
+    install)
+        cmd_install
+        cmd_reboot
+        ;;
+    uninstall)
+        cmd_uninstall
+        cmd_reboot
+        ;;
+    isinstalled)
+   	    cmd_isinstalled
+    	;;
+    enable)
+        cmd_enable
+        ;;
+    enablepermanent)
+        cmd_enable_permanent
+        ;;
+    disable)
+        cmd_disable
+        ;;
+    ispermanent)
+        cmd_ispermanent
+        ;;
+    isenabled)
+        cmd_isenabled
+        ;;
+    isloaded)
+        cmd_isloaded
+	;;
+    reboot)
+	    cmd_reboot
+	;;
+    set)
+        cmd_set $2 $3 $4
         ;;
     *)
 	    log "SBS: Unknown subcommand: $1"
 	    exit 107
 	;;
 esac
+exit $?
 
 
